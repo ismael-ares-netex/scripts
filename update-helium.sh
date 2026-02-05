ac3926a35a3dd8be1c634339d682e3341dc795ff
Ismael Ares <ismael.ares@netexlearning.com>
2026-02-05 13:23:44 +0100

    chore(update-helium): use ~

--- update-helium.sh
+++ update-helium.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-STORAGE_DIR="/home/ismael.ares/Documentos/AppImages"
-BIN_DIR="/home/ismael.ares/.bin"
+STORAGE_DIR="~/Documentos/AppImages"
+BIN_DIR="~/.bin"
 REPO="imputnet/helium-linux"
 EXE_NAME="helium"
 
